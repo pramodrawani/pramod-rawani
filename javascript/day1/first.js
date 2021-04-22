0315443d4c06a6fb8d63b961adbf0893afd9de19
Your Name <pramodrawani1995@gmail.com>
2021-04-23 01:03:05 +0530

    javascript first day

---
+++ javascript/day1/first.js
@@ -0,0 +1,100 @@
+//es5=>Ecma Script5
+//es6=> Ecma Script6
+
+///
+es5
+////
+
+Datatypes
+
+String >"hi",'hey',"43434",'5657','454aug','true'
+Number > 76, 56.5,675767,.45555
+Boolean > true ,false
+
+
+var a = 'pramod'
+var b = 12
+var c = true
+
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+
+var d = 12.45
+var e = "3455"
+
+
+
+typeof(d)
+"number"
+typeof(e)
+"string"
+
+
+//////////////////////////////////
+var a = 23
+var b = 45
+undefined
+a+b
+68
+a-b
+-22
+a*b
+1035
+a/b
+0.5111111111111111
+
+b%a >modulus
+a%b
+23>modulus of smaller to higher is always smaller
+
+
+
+
+/////
+var a = "hi"
+var b = "javascript"
+
+
+undefined
+a+b
+"hijavascript"  >concat
+a_b
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+////////
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+"10"+"20"
+"1020"
+10+"20"
+"1020"
+"23"+12
+"2312"
+12+23
+35
+
+12+"34"+34-2
+123432
+10+23+"44"-1
+3343
+"12"-1
+11
+"12"+1
+"121"
+"10a"-1
+NaN
+
+
